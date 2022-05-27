c89829b726c065670081b09fca12be18633bbab9
marina.lima <marinalimasc@gmail.com>
2022-05-27 19:00:24 -0300

    Vetores com While e For: Exercício 1 e Exercício 2 completos

--- Entra21.ExerciciosVetorWhile/Program.cs
+++ Entra21.ExerciciosVetorWhile/Program.cs
@@ -16,9 +16,22 @@ var opcaoDesejada = Convert.ToInt32(Console.ReadLine());
 
 if (opcaoDesejada == 1)
 {
-    Exercicio01 exercicio01 = new Exercicio01(); 
+    Exercicio01 exercicio01 = new Exercicio01();
     exercicio01.Executar();
+} else if (opcaoDesejada == 2)
+{
+    Exercicio02 exericicio02 = new Exercicio02();
+    exericicio02.Executar();
+
+} else if (opcaoDesejada == 3)
+{
+
+    Exercicio03 exercicio03 = new Exercicio03();
+
+
 }
+
+
 
 
  
