06ffb8087ecde4dc1e9c797118147bbe43ed4e38
marina.lima <marinalimasc@gmail.com>
2022-05-26 22:06:10 -0300

    Exercício01 - Vetores com While

---
+++ Entra21.ExerciciosVetorWhile/Exercicio01.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entra21.ExerciciosVetorWhile
+{
+    internal class Exercicio01
+    {
+        public void Executar()
+        {
+            int[] numeros = new int[5];
+
+            int indice = 0;
+            int soma = 0;
+
+            while (indice < numeros.Length)
+            {
+                Console.WriteLine("Informe um número");
+                numeros[indice] = Convert.ToInt32(Console.ReadLine());
+                soma = soma + numeros[indice];
+                indice++;
+            }
+            Console.WriteLine("soma:" + soma);
+        }
+    }
+}
+
--- Entra21.ExerciciosVetorWhile/Program.cs
+++ Entra21.ExerciciosVetorWhile/Program.cs
@@ -1,4 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Entra21.ExerciciosVetorWhile;
 
-Console.WriteLine("Oiiii");
+
+
+Console.WriteLine(@"
+  __________________________
+ |           MENU           |      
+ |                          |
+ |Exercício 1               |
+ |                          |
+ |");
+ 
+Console.WriteLine("Informe a opção desejada: ");
+var opcaoDesejada = Convert.ToInt32(Console.ReadLine());
+
+
+if (opcaoDesejada == 1)
+{
+    Exercicio01 exercicio01 = new Exercicio01(); 
+    exercicio01.Executar();
+}
+
+
+ 
