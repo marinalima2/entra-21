eca0e9608567f3ed4550d02f3c8a7f00a3774f1e
marina.lima <marinalimasc@gmail.com>
2022-05-27 21:54:29 -0300

    Exercícios Orientação a Objetos : Exercício 1 Incompleto

---
+++ ExerciciosOrientacaoObjetos/Exercicio01/ExercicioLivro.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entra21.ExerciciosOrientacaoObjetos.Exercicio01
+{
+    public class ExercicioLivro
+    {
+        public void Executar()
+        {
+           Livro livro = new Livro();
+           Console.WriteLine("Informe o nome do livro: ");
+           livro.Titulo = Console.ReadLine();
+           Console.WriteLine("Informe o autor: ");
+           livro.Autor = Console.ReadLine();
+           Console.WriteLine("Informe o código ISBN: ");
+           livro.CodigoIsbn = Convert.ToInt32(Console.ReadLine());
+           Console.WriteLine("Informe o idioma original: ");
+           livro.IdiomaOriginal = Console.ReadLine();
+           Console.WriteLine("Informe a data de lançamento: ");
+           livro.DataLancamento = Convert.ToDateTime(Console.ReadLine());
+           Console.WriteLine("Informe a quantidade de páginas: ");
+           livro.QuantidadePaginas = Convert.ToInt32(Console.ReadLine());
+           Console.WriteLine("Informe a quantidade de páginas lidas: ");
+           livro.QuantidadePaginasLidas = Convert.ToInt32(Console.ReadLine());
+           Console.WriteLine("Informe quantas releituras: ");
+           livro.Releitura = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Título e autor: " + livro.ObterTituloAutor());
+
+
+        }
+
+
+
+    }
+}
---
+++ ExerciciosOrientacaoObjetos/Program.cs
@@ -0,0 +1,25 @@
+﻿using Entra21.ExerciciosOrientacaoObjetos;
+using Entra21.ExerciciosOrientacaoObjetos.Exercicio01;
+
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine(@"
+ ___________________________
+|Exercício 1 - Digite 1     |
+|Exercício 2 - Digite 2     |
+|Exercício 3 - Digite 3     |
+|Exercício 4 - Digite 4     |
+|Exercício 5 - Digite 5     |
+|___________________________|
+
+");
+
+Console.WriteLine("Informe uma opçaõ: ");
+int opcaoDesejada = 1;
+
+if (opcaoDesejada == 1)
+{
+    ExercicioLivro exercicioLivro = new ExercicioLivro();
+    exercicioLivro.Executar();
+
+
+}
