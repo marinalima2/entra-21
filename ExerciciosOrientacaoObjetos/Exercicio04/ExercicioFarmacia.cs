c3c75a836f987e8cba2f1c4b53c3dc93cb1c95d2
marina.lima <marinalimasc@gmail.com>
2022-06-01 19:26:41 -0300

    Exercicio 4 incompleto

---
+++ ExerciciosOrientacaoObjetos/Exercicio04/ExercicioFarmacia.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entra21.ExerciciosOrientacaoObjetos.Exercicio04
+{
+    public class ExercicioFarmacia
+        
+    {
+
+        public void Executar()
+        {
+
+
+            Farmacia farmacia = new Farmacia();
+
+
+            Console.WriteLine("Informe a razão social da farmácia: ");
+            farmacia.RazaoSocial = Console.ReadLine();
+            Console.WriteLine("Informe o CNPJ: ");
+            farmacia.Cnpj = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("Informe o nome do 1° produto: ");
+            farmacia.NomeProduto1 = Console.ReadLine();
+            Console.WriteLine("Informe o valor do 1° produto: ");
+            farmacia.PrecoProduto1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Informe a categoria do 1° produto: ");
+
+            Console.WriteLine("Informe o nome do ");
+
+
+
+
+
+
+
+        }
+
+
+
+
+
+
+    }
+}
